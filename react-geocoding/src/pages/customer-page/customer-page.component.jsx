ab5b68bb31e9f8355ff66516d85f893a2dd431ad
Thomas <th.baier@posteo.de>
2020-03-29 19:13:04 +0200

    minor adjustments

--- react-geocoding/src/pages/customer-page/customer-page.component.jsx
+++ react-geocoding/src/pages/customer-page/customer-page.component.jsx
@@ -4,7 +4,7 @@ import CustomerGrid from '../../components/customer-grid/customer-grid.component
 function CustomerPage() {
     return (
         <div className="customer-page">
-            <h1>Hello Doctari</h1>
+            <h1>Customer Data Test Project</h1>
             <p>This is the Coding Challenge for Full-Stack Developer:</p>
             <CustomerGrid></CustomerGrid>
         </div>
